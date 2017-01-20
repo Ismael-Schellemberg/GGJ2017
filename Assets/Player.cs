506aef55f662dd62227ac222f5f546bc8fa8a434
Iuval <iuvalgoldansky@gmail.com>
2017-01-20 19:58:19 -0300

    Add player script

---
+++ Assets/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float xSpeed;
+
+    Vector2 position;
+
+    void Start() {
+        position = Vector2.zero;
+    }
+
+    void Update() {
+        position.x += xSpeed * Time.deltaTime;
+        transform.position = position;
+
+        if(Input.GetKeyDown(KeyCode.Space))
+            xSpeed = -xSpeed;
+    }
+}
