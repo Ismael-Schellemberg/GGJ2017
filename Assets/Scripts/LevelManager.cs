c3afde15f62dc1ce647c4793d8493406c9c8c67e
Kef Sensei <kefsensei@Kefs-MacBook-Pro.local>
2017-01-20 20:14:40 -0300

    player

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	[SerializeField] GameObject wallPrefab;
+	[SerializeField] Camera camera;
+	[SerializeField] Player player;
+	[SerializeField] float speed;
+
+	private List<GameObject> leftWalls = new List<GameObject>();
+	private List<GameObject> rightWalls = new List<GameObject>();
+
+
+
+	void Start() {
+		
+	}
+
+	void Update() {
+//		float deltaY = speed * Time.deltaTime;
+//		Vector3 movement = new Vector3 (0f, deltaY, 0f);
+//		player.transform.Translate (movement);
+//		camera.transform.Translate (movement);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public Camera camera;
+	public float xSpeed;
+	public float ySpeed;
+
+
+    void Start() {
+    }
+
+    void Update() {
+		float deltaX = xSpeed * Time.deltaTime;
+		float deltaY = ySpeed * Time.deltaTime;
+		Vector3 movement = new Vector3 (deltaX, deltaY, 0f);
+		Vector3 cameraMovement = new Vector3 (0f, deltaY, 0f);
+
+
+
+		transform.Translate (movement);
+		camera.transform.Translate (cameraMovement);
+
+		Debug.Log ("movement = " + movement + ", newPos = " + transform.position);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            xSpeed = -xSpeed;
+
+		if (Input.GetKeyDown (KeyCode.UpArrow))
+			ySpeed++;
+		else if (Input.GetKeyDown (KeyCode.DownArrow))
+			ySpeed--;
+    }
+}
